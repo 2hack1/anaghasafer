b017861331b9c2b5354a45eb8384df3e1fc82653
ayushmay <Ayushsharma014may@gmail.com>
2025-05-04 15:59:55 +0530

    update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,9 +11,9 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimations(),
-    // {
-    //   provide: APP_BASE_HREF,
-    //   useValue: '/'
-    // }
+    {
+      provide: APP_BASE_HREF,
+      useValue: '/'
+    }
   ]
 };
