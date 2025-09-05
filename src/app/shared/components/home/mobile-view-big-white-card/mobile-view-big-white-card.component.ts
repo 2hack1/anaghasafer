0c1e814d8c5fc5d1baad685d01057a9107f76d49
2hack1 <kapilagrawal230@gmail.com>
2025-09-05 17:54:09 +0530

    update on home page

--- src/app/shared/components/home/mobile-view-big-white-card/mobile-view-big-white-card.component.ts
+++ src/app/shared/components/home/mobile-view-big-white-card/mobile-view-big-white-card.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, HostListener, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AxiosService } from '../../../../core/services/axios/axios.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-mobile-view-big-white-card',
@@ -16,8 +18,9 @@ export class MobileViewBigWhiteCardComponent implements OnInit {
   // Other fields
   city = 'Goa';
   cityOptions: string[] = ['Delhi', 'Mumbai', 'Bangalore', 'Goa'];
-  country = 'India'; checkIn: ''
-  checkOut: ''
+  country = 'India';
+   checkIn: '';
+  checkOut: '';
   totalGuests = 2; 
   totalRooms = 1;
   // Modals 
@@ -32,7 +35,21 @@ export class MobileViewBigWhiteCardComponent implements OnInit {
   // for tour
   
   ngOnInit(): void {
+    document.addEventListener("click", () => {
+    this.check = false;
+    this.checkHotel=false;
+  });
     this.checkScreenSize();
+      this.service.getPackagePlaceName()
+    .then((res: any) => {
+      console.log("check city", res.data.data);
+
+      // Add cities dynamically to dropdown
+      this.cityOptionsTour.push(...res.data.data.map((item: any) => item.city_name ?? item.place_name));
+    })
+    .catch((err: any) => {
+      console.error("Error fetching city names", err);
+    });
   }
   // ✅ Detect mobile 
   @HostListener('window:resize') onResize() {
@@ -46,16 +63,40 @@ export class MobileViewBigWhiteCardComponent implements OnInit {
   decChildren() { if (this.children > 0) this.children--; this.updateGuests(); }
   incRooms() { if (this.rooms < this.maxRooms) this.rooms++; this.updateGuests(); }
   decRooms() { if (this.rooms > 1) this.rooms--; this.updateGuests(); }
+
   updateGuests() { this.totalGuests = this.adults + this.children; this.totalRooms = this.rooms; }
   applyGuests() { this.showGuestsModal = false; }
   // ✅ Price range increment/decrement 
-  incMinPrice() { if (this.minPrice + 500 <= this.maxPrice) this.minPrice += 500; }
-  decMinPrice() { if (this.minPrice - 500 >= 0) this.minPrice -= 500; }
-  incMaxPrice() { if (this.maxPrice + 500 <= 50000) this.maxPrice += 500; }
-  decMaxPrice() { if (this.maxPrice - 500 >= this.minPrice) this.maxPrice -= 500; }
   applyPrice() { this.showPriceModal = false; }
 
-  toggleGuestsModal() { this.showGuestsModal = !this.showGuestsModal; }
+
+  incMinPrice() {
+  if (this.minPrice + 500 <= this.maxPrice && this.minPrice + 500 <= 50000) {
+    this.minPrice += 500;
+  }
+}
+
+decMinPrice() {
+  if (this.minPrice - 500 >= 0) {
+    this.minPrice -= 500;
+  }
+}
+
+incMaxPrice() {
+  if (this.maxPrice + 500 <= 50000) {
+    this.maxPrice += 500;
+  }
+}
+decMaxPrice() {
+  if (this.maxPrice - 500 >= this.minPriceTour && this.maxPrice - 500 >= 2000) {
+    this.maxPrice -= 500;
+  }
+}
+
+
+  toggleGuestsModal() { this.showGuestsModal = !this.showGuestsModal;
+   
+   }
 
   togglePriceModal() { this.showPriceModal = !this.showPriceModal; }
   search() { console.log('Searching hotels...'); }
@@ -133,7 +174,7 @@ export class MobileViewBigWhiteCardComponent implements OnInit {
   today = new Date().toISOString().split('T')[0];
   // today's date in yyyy-mm-dd
   //  // slides
-  currentSlide: number = 1;
+  currentSlide: number = 0;
   goToSlide(index: number) {
     this.currentSlide = index;
   }
@@ -142,13 +183,28 @@ export class MobileViewBigWhiteCardComponent implements OnInit {
   tempCheckOut: string = '';
 
   showDateModal: boolean = false;
-  constructor() {
+  constructor(private service:AxiosService,private route:Router) {
 
     const today = new Date();
     const tomorrow = new Date(today);
-    tomorrow.setDate(today.getDate() + 1); this.hotel.checkIn = this.formatDate(today); this.hotel.checkOut = this.formatDate(tomorrow); this.tempCheckIn = this.hotel.checkIn; this.tempCheckOut = this.hotel.checkOut;
+    tomorrow.setDate(today.getDate() + 1); 
+    this.hotel.checkIn = this.formatDate(today); 
+    this.hotel.checkOut = this.formatDate(tomorrow);
+     this.tempCheckIn = this.hotel.checkIn;
+      this.tempCheckOut = this.hotel.checkOut;
   } /** Open date selection modal */
-  openDateModal(): void { this.showDateModal = true; this.tempCheckIn = this.hotel.checkIn; this.tempCheckOut = this.hotel.checkOut; } /** Close date modal without saving changes */ closeDateModal(): void { this.showDateModal = false; } /** Close modal from "X" button */ PriceModal(): void { this.showDateModal = false; } /** Apply selected dates */ applyDates(): void {
+  openDateModal(): void {
+     this.showDateModal = true;
+      this.tempCheckIn = this.hotel.checkIn; 
+      this.tempCheckOut = this.hotel.checkOut;
+     } /** Close date modal without saving changes */ 
+     closeDateModal(): void { 
+      this.showDateModal = false;
+     } /** Close modal from "X" button */
+      PriceModal(): void { 
+        this.showDateModal = false; 
+      } /** Apply selected dates */ 
+      applyDates(): void {
     if (!this.tempCheckIn || !this.tempCheckOut) { alert('Please select both Check-In and Check-Out dates.'); return; } const checkInDate = new Date(this.tempCheckIn); const checkOutDate = new Date(this.tempCheckOut);
     // Validation: check-out date must be after check-in
     if (checkOutDate <= checkInDate) { alert('Check-out date must be after Check-in date.'); return; }
@@ -170,7 +226,8 @@ export class MobileViewBigWhiteCardComponent implements OnInit {
   // for hotel slide 
   hotelCity: string = '';
   hotelCityOptions: string[] = ['Goa', 'Delhi', 'Mumbai', 'Jaipur'];
-  hotelCountry: string = 'India'; hotelBooking = { checkIn: '', checkOut: '' };
+  hotelCountry: string = 'India'; 
+  hotelBooking = { checkIn: '', checkOut: '' };
   showHotelDateModal = false; tempHotelCheckIn: string = '';
   tempHotelCheckOut: string = ''; hotelTotalGuests: number = 2;
   hotelTotalRooms: number = 1; hotelMinPrice: number = 1000;
@@ -217,15 +274,15 @@ export class MobileViewBigWhiteCardComponent implements OnInit {
   applyMonth() { this.showMonthModal = false; console.log('Selected Month:', this.selectedMonth); }
 
 
-// ********************************************************************************************************************
+// ************************************  Tour working********************************************************************************
 
-  // --------- TOUR MODAL VARIABLES ----------
+// --------- adults and children  ----------
 adultsTour = 2;
-childrenTour = 0;
+childrenTour = 1;
 roomsTour = 1;
 
-maxAdultsTour = 5;
-maxChildrenTour = 5;
+maxAdultsTour = 100;
+maxChildrenTour = 100;
 maxRoomsTour = 5;
 
 totalGuestsTour = 2;
@@ -266,27 +323,180 @@ decRoomsTour() {
 
 updateGuestsTour() {
   this.totalGuestsTour = this.adultsTour + this.childrenTour;
+
   this.totalRoomsTour = this.roomsTour;
 }
 
 applyGuestsTour() {
   this.showGuestsModalTour = false;
+
 }
 
 toggleGuestsModalTour() {
   this.showGuestsModalTour = !this.showGuestsModalTour;
+
+}
+// *************
+
+
+// ===== Tour Price Modal Variables =====
+showPriceModalTour: boolean = false;
+minPriceTour: number = 0;
+maxPriceTour: number = 1500;
+
+// ===== Tour Price Modal Functions =====
+togglePriceModalTour() {
+  this.showPriceModalTour = !this.showPriceModalTour;
+}
+
+incMinPriceTour() {
+  if (this.minPriceTour + 500 <= this.maxPriceTour && this.minPriceTour + 500 <= 50000) {
+    this.minPriceTour += 500;
+  }
+}
+
+decMinPriceTour() {
+  if (this.minPriceTour - 500 >= 0) {
+    this.minPriceTour -= 500;
+  }
+}
+
+incMaxPriceTour() {
+  if (this.maxPriceTour + 500 <= 50000) {
+    this.maxPriceTour += 500;
+  }
+}
+decMaxPriceTour() {
+  if (this.maxPriceTour - 500 >= this.minPriceTour && this.maxPriceTour - 500 >= 2000) {
+    this.maxPriceTour -= 500;
+  }
+}
+
+applyPriceTour() {
+  this.togglePriceModalTour(); // closes modal after applying
+
 }
 
 
+// ***********************************************
+// ===== Tour City Dropdown Variables =====
+cityTour: string = 'Delhi';
+cityOptionsTour: string[] = ['Delhi'];
+countryTour: string = 'India';
+
+// ===== Tour City Functions =====
+onCityChangeTour() {
+   this.countryTour = 'India';
+  // Example: auto-update country based on city
+  switch (this.cityTour) {
+    case 'Delhi':
+    case 'Mumbai':
+    case 'Bangalore':
+    case 'Goa':
+      this.countryTour = 'India';
+      break;
+    // you can add more mappings here
+    default:
+      this.countryTour = 'Unknown';
+  }
+}
+
+// ************************************
 search0(){
-alert(' destination uder working ')
+  console.log("city",this.cityTour)
+  console.log("cityOptionsTour",this.cityOptionsTour)
+
+  console.log("minPriceTour",this.minPriceTour);
+  console.log("maxPriceTour",this.maxPriceTour);
+  console.log('adults',this.adultsTour);
+  console.log('childrenTour',this.childrenTour);
+  console.log('roomsTour',this.roomsTour);
+
+
+
+   this.route.navigate(['/dss'], {
+    queryParams: {
+      selectedMonth:  this.selectedMonth,
+      destination: this.cityTour,
+      adults:this.adultsTour,
+      children: this.childrenTour,
+      days: this.roomsTour,
+      minPrice:this.minPriceTour ,
+      maxPrice: this.maxPriceTour
+    }
+  });
+
 }
 
 search1(){
-  alert(' hotle uder working')
+ 
+ console.log("city",this.city)
+  console.log("minPrice",this.minPrice);
+  console.log("maxPrice",this.maxPrice);
+  console.log('adults',this.adultsTour);
+  console.log('children',this.children);
+  console.log('rooms',this.rooms);
+  console.log('checkIn',this.hotel.checkIn);
+  console.log('checkOut',this.hotel.checkOut);
+
 }
 
 search2(){
   alert(' train uder working')
 }
+
+
+
+// ****************************
+citySearch: string = '';
+
+// Filter list based on search
+filteredCityOptionsTour() {
+  return this.cityOptionsTour.filter(c =>
+    c.toLowerCase().includes(this.citySearch.toLowerCase())
+  );
+}
+
+// When selecting a city
+selectCityTour(city: string) {
+  this.cityTour = city;
+  this.citySearch = city; // optional: put selected city into input
+  this.onCityChangeTour();
+  this.check=false;
+
+
+}
+check:boolean=false;
+openDropdown(event: Event) {
+  this.check = true; // always open when clicked
+  event.stopPropagation(); // don’t bubble to document
+}
+// ***************************search for hotel***********
+
+
+
+citySearchHotel: string = '';
+
+// Filter list based on search
+filteredCityOptionsHotel() {
+  return this.cityOptions.filter(c =>
+    c.toLowerCase().includes(this.citySearchHotel.toLowerCase())
+  );
+}
+
+// When selecting a city
+selectCityHotel(city: string) {
+  this.city = city;
+  this.citySearchHotel = city; // optional: put selected city into input
+  this.onCityChangeTour();
+  this.checkHotel=false;
+}
+checkHotel:boolean=false;
+openDropdownHotel(event: Event) {
+  this.checkHotel = true; // always open when clicked
+  event.stopPropagation(); // don’t bubble to document
+}
+
+
+
 }
